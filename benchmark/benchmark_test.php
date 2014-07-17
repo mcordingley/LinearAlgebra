647e62166962dbb1cd932556bfde38f52def8b44
James T <james.thomin@gmail.com>
2014-07-16 21:15:49 -0500

    changed benchmark test to use a configuration array to determine matrix sizes, and unset matrices in between tests

--- benchmark/benchmark_test.php
+++ benchmark/benchmark_test.php
@@ -6,12 +6,17 @@ require_once '../vendor/autoload.php';
 
 // Set up the benchmark class
 $benchmark = new Benchmark();
-$n = 2000;
-$step = 10;
-$start = 2;
+
+$config = array(
+	5 => 10,
+    10 => 5,
+    100 => 1,
+    200 => 1
+);
+
 
 // Loop over matrix sizes
-for($size = $start; $size <= $n; $size *= $step) {
+foreach($config as $size => $repeat) {
     $random_numbers1 = array();
     $random_numbers2 = array();
     $seed = 0;
@@ -34,13 +39,23 @@ for($size = $start; $size <= $n; $size *= $step) {
     $x = $m1->add(25);
     $benchmark->end($key);
     
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
     // add a matrix
     $key = "Add a matrix, size = $size";
     echo $key."\n";
     $benchmark->start($key);
     $x = $m1->add($m2);
     $benchmark->end($key);
-    
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+
     // subtract a scalar
     $key = "Subtract a scalar, size = $size";
     echo $key."\n";
@@ -48,12 +63,22 @@ for($size = $start; $size <= $n; $size *= $step) {
     $x = $m1->subtract(25);
     $benchmark->end($key);
 
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
     // subtract a matrix
     $key = "Subtract a matrix, size = $size";
     echo $key."\n";
     $benchmark->start($key);
     $x = $m1->subtract($m2);
     $benchmark->end($key);
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
     
     // multiply by scalar
     $key = "Multiply a scalar, size = $size";
@@ -61,6 +86,12 @@ for($size = $start; $size <= $n; $size *= $step) {
     $benchmark->start($key);
     $x = $m1->multiply(25);
     $benchmark->end($key);
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
 /**    
     // multiply by matrix
     $key = "Multiply a matrix, size = $size";
@@ -69,12 +100,24 @@ for($size = $start; $size <= $n; $size *= $step) {
     $x = $m1->multiply($m2);
     $benchmark->end($key);
 **/
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
+    
     // trace
     $key = "Trace, size = $size";
     echo $key."\n";
     $benchmark->start($key);
     $x = $m1->trace();
     $benchmark->end($key);
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
     
     // transpose
     $key = "Transpose, size = $size";
@@ -82,21 +125,36 @@ for($size = $start; $size <= $n; $size *= $step) {
     $benchmark->start($key);
     $x = $m1->transpose();
     $benchmark->end($key);
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
     
     // adjoint
-    /**
     $key = "Adjoint, size = $size";
     echo $key."\n";
     $benchmark->start($key);
     $x = $m1->adjoint();
     $benchmark->end($key);
-    **/
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
+    
     // determinant
     $key = "Determinant, size = $size";
     echo $key."\n";
     $benchmark->start($key);
     $x = $m1->determinant();
     $benchmark->end($key);
+
+    unset($m1);
+    unset($m2);
+    $m1 = new Matrix($random_numbers1);
+    $m2 = new Matrix($random_numbers2);
     
     // inverse
     $key = "Inverse, size = $size";
