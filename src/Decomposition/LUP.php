6029743d1eb00b7b471d0bd700da33c2d01aa7bc
Michael Cordingley <Michael.Cordingley@gmail.com>
2016-11-24 17:07:21 -0500

    LUP decomposition gets to live in its own class.

---
+++ src/Decomposition/LUP.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace mcordingley\LinearAlgebra\Decomposition;
+
+use mcordingley\LinearAlgebra\Matrix;
+use mcordingley\LinearAlgebra\MatrixException;
+
+final class LUP
+{
+    /** @var Matrix */
+    private $decomposition;
+
+    /** @var array */
+    private $permutation;
+
+    /**
+     * @param Matrix $source
+     * @throws MatrixException
+     */
+    public function __construct(Matrix $source)
+    {
+        $this->decompose($source);
+    }
+
+    /**
+     * @param Matrix $source
+     * @throws MatrixException
+     */
+    private function decompose(Matrix $source)
+    {
+        $sourceLiteral = $source->toArray();
+        $decompositionLiteral = $sourceLiteral;
+
+        if (!$source->isSquare()) {
+            throw new MatrixException('Operation can only be called on square matrix: ' . print_r($decompositionLiteral, true));
+        }
+
+        $size = $source->getRowCount();
+        $this->permutation = range(0, $size - 1);
+
+        for ($k = 0; $k < $size; $k++) {
+            $p = 0.0;
+            $kPrime = $k;
+
+            for ($i = $k; $i < $size; $i++) {
+                $absolute = abs($decompositionLiteral[$i][$k]);
+
+                if ($absolute > $p) {
+                    $p = $absolute;
+                    $kPrime = $i;
+                }
+            }
+
+            if ($p === 0.0) {
+                throw new MatrixException('Cannot take the LUP decomposition of a singular matrix: ' . print_r($sourceLiteral, true));
+            }
+
+            list($this->permutation[$k], $this->permutation[$kPrime]) = [$this->permutation[$kPrime], $this->permutation[$k]];
+
+            for ($i = 0; $i < $size; $i++) {
+                list($decompositionLiteral[$k][$i], $decompositionLiteral[$kPrime][$i]) = [$decompositionLiteral[$kPrime][$i], $decompositionLiteral[$k][$i]];
+            }
+
+            for ($i = $k + 1; $i < $size; $i++) {
+                $decompositionLiteral[$i][$k] = $decompositionLiteral[$i][$k] / $decompositionLiteral[$k][$k];
+
+                for ($j = $k + 1; $j < $size; $j++) {
+                    $decompositionLiteral[$i][$j] = $decompositionLiteral[$i][$j] - $decompositionLiteral[$i][$k] * $decompositionLiteral[$k][$j];
+                }
+            }
+        }
+
+        $this->decomposition = new Matrix($decompositionLiteral);
+    }
+
+    /**
+     * @return Matrix
+     */
+    public function getLower(): Matrix
+    {
+        return $this->decomposition->lower(true);
+    }
+
+    /**
+     * @return Matrix
+     */
+    public function getUpper(): Matrix
+    {
+        return $this->decomposition->upper(false);
+    }
+
+    /**
+     * @return Matrix
+     */
+    public function getPermutationMatrix(): Matrix
+    {
+        $size = count($this->permutation);
+        $permutationMatrix = [];
+
+        for ($i = 0; $i < $size; $i++) {
+            $permutationMatrix[] = [];
+
+            for ($j = 0; $j < $size; $j++) {
+                $permutationMatrix[$i][] = $this->permutation[$i] === $j ? 1 : 0;
+            }
+        }
+
+        return new Matrix($permutationMatrix);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPermutationArray(): array
+    {
+        return $this->permutation;
+    }
+}
---
+++ tests/Decomposition/LUPTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace mcordingley\LinearAlgebraTest;
+
+use mcordingley\LinearAlgebra\Decomposition\LUP;
+use mcordingley\LinearAlgebra\Matrix;
+use PHPUnit_Framework_TestCase;
+
+final class LUPTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetLower()
+    {
+        $lower = $this->getDecomposition()->getLower();
+
+        static::assertEquals(1, $lower->get(0, 0));
+        static::assertEquals(0, $lower->get(0, 1));
+        static::assertEquals(0, $lower->get(0, 2));
+        static::assertEquals(0, $lower->get(0, 3));
+
+        static::assertEquals(0.4, $lower->get(1, 0));
+        static::assertEquals(1, $lower->get(1, 1));
+        static::assertEquals(0, $lower->get(1, 2));
+        static::assertEquals(0, $lower->get(1, 3));
+
+        static::assertEquals(-0.2, $lower->get(2, 0));
+        static::assertEquals(0.5, $lower->get(2, 1));
+        static::assertEquals(1, $lower->get(2, 2));
+        static::assertEquals(0, $lower->get(2, 3));
+
+        static::assertEquals(0.6, $lower->get(3, 0));
+        static::assertEquals(0, $lower->get(3, 1));
+        static::assertEquals(0.4, $lower->get(3, 2));
+        static::assertEquals(1, $lower->get(3, 3));
+    }
+
+    private function getDecomposition(): LUP
+    {
+        $matrix = new Matrix([
+            [ 2,  0,    2, 0.6],
+            [ 3,  3,    4,  -2],
+            [ 5,  5,    4,   2],
+            [-1, -2,  3.4,  -1],
+        ]);
+
+        return new LUP($matrix);
+    }
+    public function testGetUpper()
+    {
+        $upper = $this->getDecomposition()->getUpper();
+
+        static::assertEquals(5, $upper->get(0, 0));
+        static::assertEquals(5, $upper->get(0, 1));
+        static::assertEquals(4, $upper->get(0, 2));
+        static::assertEquals(2, $upper->get(0, 3));
+
+        static::assertEquals(0, $upper->get(1, 0));
+        static::assertEquals(-2, $upper->get(1, 1));
+        static::assertEquals(0.4, $upper->get(1, 2));
+        static::assertEquals(-0.2, $upper->get(1, 3));
+
+        static::assertEquals(0, $upper->get(2, 0));
+        static::assertEquals(0, $upper->get(2, 1));
+        static::assertEquals(4, $upper->get(2, 2));
+        static::assertEquals(-0.5, $upper->get(2, 3));
+
+        static::assertEquals(0, $upper->get(3, 0));
+        static::assertEquals(0, $upper->get(3, 1));
+        static::assertEquals(0, $upper->get(3, 2));
+        static::assertEquals(-3, $upper->get(3, 3));
+    }
+
+    public function testGetPermutationArray()
+    {
+        $permutation = $this->getDecomposition()->getPermutationArray();
+
+        static::assertEquals(2, $permutation[0]);
+        static::assertEquals(0, $permutation[1]);
+        static::assertEquals(3, $permutation[2]);
+        static::assertEquals(1, $permutation[3]);
+    }
+
+    public function testGetPermutationMatrix()
+    {
+        $permutation = $this->getDecomposition()->getPermutationMatrix();
+
+        static::assertEquals(0, $permutation->get(0, 0));
+        static::assertEquals(0, $permutation->get(0, 1));
+        static::assertEquals(1, $permutation->get(0, 2));
+        static::assertEquals(0, $permutation->get(0, 3));
+
+        static::assertEquals(1, $permutation->get(1, 0));
+        static::assertEquals(0, $permutation->get(1, 1));
+        static::assertEquals(0, $permutation->get(1, 2));
+        static::assertEquals(0, $permutation->get(1, 3));
+
+        static::assertEquals(0, $permutation->get(2, 0));
+        static::assertEquals(0, $permutation->get(2, 1));
+        static::assertEquals(0, $permutation->get(2, 2));
+        static::assertEquals(1, $permutation->get(2, 3));
+
+        static::assertEquals(0, $permutation->get(3, 0));
+        static::assertEquals(1, $permutation->get(3, 1));
+        static::assertEquals(0, $permutation->get(3, 2));
+        static::assertEquals(0, $permutation->get(3, 3));
+    }
+}
