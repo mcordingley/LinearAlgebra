6d83337640aed93d4845f10e94c23c300be66887
battlecook <battlecook@com2us.com>
2017-06-02 10:24:34 +0900

    change class hierarchy in vector ( Vector inherits matrix )

--- src/Vector.php
+++ src/Vector.php
@@ -4,46 +4,30 @@ declare(strict_types = 1);
 
 namespace MCordingley\LinearAlgebra;
 
-class Vector
+class Vector extends Matrix
 {
     /**
      * Number of elements
      * @var int
      */
     private $size;
-
     /**
-     * Vector
      * @var array
      */
-    private $literal;
+    private $vector;
 
     public function __construct(array $literal)
     {
-        if (!static::isLiteralValid($literal)) {
-            throw new VectorException('Invalid array provided: ' . print_r($literal, true));
-        }
+        parent::__construct($literal);
 
-        $this->literal = $literal;
-        $this->size = count($literal);
-    }
-
-    /**
-     * @param array $literal
-     * @return boolean
-     */
-    private static function isLiteralValid(array $literal): bool
-    {
-        return $literal && isset($literal[0]);
+        $this->vector = $literal[0];
+        $this->size = count($this->vector);
     }
 
     /**************************************************************************
      * BASIC VECTOR GETTERS
      *  - getVector
      *  - getSize
-     *  - get
-     *  - asColumnMatrix
-     *  - asRowMatrix
      **************************************************************************/
 
     /**
@@ -52,7 +36,7 @@ class Vector
      */
     public function getVector(): array
     {
-        return $this->literal;
+        return $this->vector;
     }
 
     /**
@@ -62,58 +46,6 @@ class Vector
     public function getSize(): int
     {
         return $this->size;
-    }
-
-    /**
-     * @param int $i
-     * @return int
-     * @throws VectorException
-     */
-    public function get(int $i)
-    {
-        if ($i >= $this->size) {
-            throw new VectorException("Element $i does not exist");
-        }
-
-        return $this->literal[$i];
-    }
-
-    /**
-     * Get the vector as an nx1 column matrix
-     *
-     * Example:
-     *  V = [1, 2, 3]
-     *
-     *      [1]
-     *  R = [2]
-     *      [3]
-     *
-     * @return Matrix
-     */
-    public function asColumnMatrix()
-    {
-        $literal = [];
-        foreach ($this->literal as $element) {
-            $literal[] = [$element];
-        }
-
-        return new Matrix(array($literal));
-    }
-
-    /**
-     * Get the vector as a 1xn row matrix
-     *
-     * Example:
-     *  V = [1, 2, 3]
-     *  R = [
-     *   [1, 2, 3]
-     *  ]
-     *
-     * @return Matrix
-     */
-    public function asRowMatrix()
-    {
-        return new Matrix([$this->literal]);
     }
 
     /**************************************************************************
@@ -130,7 +62,7 @@ class Vector
      */
     public function sum()
     {
-        return array_sum($this->literal);
+        return array_sum($this->vector);
     }
 
     /**
@@ -145,152 +77,63 @@ class Vector
     }
 
     /**
+     *
      * Dot product (inner product) (A⋅B)
      * https://en.wikipedia.org/wiki/Dot_product
      *
-     * @param self $value
-     *
-     * @return number
+     * @param self $other
+     * @return float
+     * @throws VectorException
      */
-    public function dotProduct(self $value)
+    public function dotProduct(self $other)
     {
-        if ($value->getSize() !== $this->size) {
-            throw new VectorException('Vectors have different number of items');
+        if ($other->getSize() !== $this->size) {
+            throw new VectorException('Vectors have to have same size');
         }
 
         return array_sum(array_map(
             function ($a, $b) {
                 return $a * $b;
             },
-            $this->literal,
-            $value->getVector()
+            $this->vector,
+            $other->getVector()
         ));
     }
 
     /**
      * Inner product (convience method for dot product) (A⋅B)
      *
-     * @param Vector $B
+     * @param Vector $other
      *
      * @return number
      */
-    public function innerProduct(Vector $B)
+    public function innerProduct(Vector $other)
     {
-        return $this->dotProduct($B);
+        return $this->dotProduct($other);
     }
 
     /**************************************************************************
      * VECTOR OPERATIONS - Return a Vector or Matrix
-     *  - add
-     *  - subtract
-     *  - scalarMultiply
-     *  - scalarDivide
      *  - outerProduct
      *  - normalize
      *  - projection
      **************************************************************************/
 
     /**
-     * Add (A + B)
-     *
-     * A = [a₁, a₂, a₃]
-     * B = [b₁, b₂, b₃]
-     * A + B = [a₁ + b₁, a₂ + b₂, a₃ + b₃]
-     *
-     * @param self $other
-     *
-     * @return self
-     */
-    public function addVector(self $other): self
-    {
-        if ($other->getSize() !== $this->size) {
-            throw new VectorException('Vectors must be the same length for addition');
-        }
-
-        $sums = array_fill(0, $this->size, 0);
-        for($i = 0; $i < $this->size; $i++) {
-            $sums[$i] = $other->literal[$i] + $this->literal[$i];
-        }
-
-        return new Vector($sums);
-    }
-
-    /**
-     * Subtract (A - B)
-     *
-     * A = [a₁, a₂, a₃]
-     * B = [b₁, b₂, b₃]
-     * A - B = [a₁ - b₁, a₂ - b₂, a₃ - b₃]
-     *
-     * @param self $other
-     *
-     * @return self
-     */
-    public function subtractVector(Vector $other): self
-    {
-        if ($other->getSize() !== $this->size) {
-            throw new VectorException('Vectors must be the same length for addition');
-        }
-
-        $subtracts = array_fill(0, $this->size, 0);
-        for($i = 0; $i < $this->size; $i++) {
-            $subtracts[$i] =  $this->literal[$i] - $other->literal[$i];
-        }
-
-        return new Vector($subtracts);
-    }
-
-    /**
-     * Scalar multiplication (scale)
-     * kA = [k * a₁, k * a₂, k * a₃ ...]
-     *
-     * @param number $value Scale factor
-     *
-     * @return self
-     */
-    public function scalarMultiply($value): self
-    {
-        $scalarMultiplies = array();
-        for($i = 0; $i < $this->size; $i++) {
-            $scalarMultiplies[$i] =  $this->literal[$i] * $value;
-        }
-
-        return new Vector($scalarMultiplies);
-    }
-
-    /**
-     * Scalar divide
-     * kA = [k / a₁, k / a₂, k / a₃ ...]
-     *
-     * @param number $value Scale factor
-     *
-     * @return self
-     */
-    public function scalarDivide($value): self
-    {
-        $scalarDivide = array();
-        for($i = 0; $i < $this->size; $i++) {
-            $scalarDivide[$i] =  $this->literal[$i] / $value;
-        }
-
-        return new Vector($scalarDivide);
-    }
-
-    /**
      * Outer product (A⨂B)
      * https://en.wikipedia.org/wiki/Outer_product
      * Same as direct product.
      *
-     * @param Vector $value
+     * @param Vector $other
      *
      * @return Matrix
      */
-    public function outerProduct(Vector $value): Matrix
+    public function outerProduct(Vector $other): Matrix
     {
         $R = [];
         for ($i = 0; $i < $this->size; $i++) {
-            for ($j = 0; $j < $value->getSize(); $j++) {
-                $R[$i][$j] = $this->literal[$i] * $value[$j];
+            for ($j = 0; $j < $other->getSize(); $j++) {
+                $R[$i][$j] = $this->internal[$i] * $other[$j];
             }
         }
 
@@ -309,13 +152,12 @@ class Vector
      *
      *  where |A| is the l²-norm (|A|₂)
      *
-     * @return self
      */
-    public function normalize(): self
+    public function normalize()
     {
         $norm = $this->l2norm();
 
-        return $this->scalarDivide($norm);
+        return $this->divideScalar($norm);
     }
 
     /**
@@ -326,13 +168,13 @@ class Vector
      * projᵇA = --- B
      *          |B|²
      *
-     * @param self $value
+     * @param self $other
      *
      * @return self
      */
-    public function projection(self $value): self
+    public function projection(self $other)
     {
-        return $value->scalarMultiply($this->dotProduct($value) / (($value->l2norm())**2));
+        return $other->multiplyScalar($this->dotProduct($other) / (($other->l2norm())**2));
     }
 
     /**************************************************************************
@@ -355,7 +197,7 @@ class Vector
     public function l1Norm()
     {
         $sum = 0;
-        foreach($this->literal as $value) {
+        foreach($this->vector as $value) {
             $sum += abs($value);
         }
 
@@ -372,13 +214,13 @@ class Vector
      *         ______
      * |x|₂ = √∑|xᵢ|²
      *
-     * @return number
+     * @return float
      */
     public function l2Norm()
     {
         $literal = array();
         for ($i = 0, $rows = $this->getSize(); $i < $rows; $i++) {
-            $literal[] = $this->literal[$i]**2;
+            $literal[] = $this->vector[$i]**2;
         }
 
         return sqrt(array_sum($literal));
@@ -393,8 +235,8 @@ class Vector
      */
     public function maxNorm()
     {
-        $max = abs($this->literal[0]);
-        foreach($this->literal as $value) {
+        $max = abs($this->vector[0]);
+        foreach($this->vector as $value) {
             if(abs($value) > $max) {
                 $max = abs($value);
             }
--- tests/VectorTest.php
+++ tests/VectorTest.php
@@ -1,19 +1,21 @@
 <?php
 namespace MathPHP\Tests\LinearAlgebra;
 
+use MCordingley\LinearAlgebra\MatrixException;
 use MCordingley\LinearAlgebra\Vector;
-use MCordingley\LinearAlgebra\VectorException;
 
 class VectorTest extends \PHPUnit_Framework_TestCase
 {
     private function buildVector()
     {
-        return new Vector([1, 2, 3, 4]);
+        return new Vector([
+            [1, 2, 3, 4],
+        ]);
     }
 
     public function testEmptyCount()
     {
-        static::expectException(VectorException::class);
+        static::expectException(MatrixException::class);
 
         new Vector([]);
     }
@@ -23,18 +25,11 @@ class VectorTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Vector::class, self::buildVector());
     }
 
-    public function getSize()
+    public function testGetSize()
     {
         $vector = self::buildVector();
 
         $this->assertEquals(4, $vector->getSize());
-    }
-
-    public function testGet()
-    {
-        $vector = self::buildVector();
-
-        $this->assertEquals(2, $vector->get(1));
     }
 
     public function testSum()
@@ -46,54 +41,37 @@ class VectorTest extends \PHPUnit_Framework_TestCase
 
     public function testLength()
     {
-        $vector = new Vector([3, 4]);
+        $vector = new Vector([
+            [3, 4]
+        ]);
 
         $this->assertEquals(5, $vector->length());
     }
 
     public function testDotProduct()
     {
-        $vector1 = new Vector([1,2,3,4]);
-        $vector2 = new Vector([-1,3,5,2]);
+        $vector1 = new Vector([
+            [1,2,3,4]
+        ]);
+        $vector2 = new Vector([
+            [-1,3,5,2]
+        ]);
 
         $this->assertEquals((1 * -1) + (2 * 3) + (3 * 5) + (4 * 2), $vector1->dotProduct($vector2));
     }
 
-    public function testAddVector()
+    public function testOuterProduct()
     {
-        $vector1 = new Vector([1,2,3,4]);
-        $vector2 = new Vector([-1,3,5,2]);
 
-        $this->assertEquals(new Vector([(1 + (-1)),  (2 + 3),  (3 + 5), (4 + 2)]), $vector1->addVector($vector2));
-    }
-
-    public function testSubtractVector()
-    {
-        $vector1 = new Vector([1,2,3,4]);
-        $vector2 = new Vector([-1,3,5,2]);
-
-        $this->assertEquals(new Vector([(1 - (-1)),  (2 - 3),  (3 - 5), (4 - 2)]), $vector1->subtractVector($vector2));
-    }
-
-    public function testScalarMultiply()
-    {
-        $vector = self::buildVector();
-
-        $this->assertEquals(new Vector([(1*3),  (2*3),  (3*3), (4*3)]), $vector->scalarMultiply(3));
-    }
-
-    public function testScalarDivide()
-    {
-        $vector = self::buildVector();
-
-        $this->assertEquals(new Vector([(1/3),  (2/3),  (3/3), (4/3)]), $vector->scalarDivide(3));
     }
 
     public function testNormalize()
     {
-        $vector = new Vector([3,4]);
+        $vector = new Vector([
+            [3,4]
+        ]);
 
-        $this->assertEquals(new Vector([3/5, 4/5]), $vector->normalize());
+        $this->assertEquals(new Vector([[3/5, 4/5]]), $vector->normalize());
     }
 
     public function testL1Norm()
@@ -103,8 +81,15 @@ class VectorTest extends \PHPUnit_Framework_TestCase
 
     public function testL2Norm()
     {
-        $vector = new Vector([3, 4]);
+        $vector = new Vector([
+            [3, 4]
+        ]);
 
         $this->assertEquals(5, $vector->l2Norm());
+    }
+
+    public function testProjection()
+    {
+
     }
 }
